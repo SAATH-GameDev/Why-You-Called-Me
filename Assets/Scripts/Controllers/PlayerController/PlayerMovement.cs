c08ac2095bc3390dc13fd341f7cffd4459e8ea9e
bilalCh213 <programmercheema@gmail.com>
2024-08-15 20:15:22 +0500

    Added Player Head Bobbing

--- Assets/Scripts/Controllers/PlayerController/PlayerMovement.cs
+++ Assets/Scripts/Controllers/PlayerController/PlayerMovement.cs
@@ -9,17 +9,28 @@ public class PlayerMovement : MonoBehaviour
     public float jumpHeight = 3f;
     public float sprintSpeed = 18f;
 
+    [Space]
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    [Space]
+    public AnimationCurve bobbingYCurve;
+    public float bobbingYLength = 0.5f;
+    public float bobbingYRate = 0.5f;
+
     Vector3 velocity;
     bool isGrounded;
     float currentSpeed;
+    float bobbingYTimer = 0.0f;
+    Transform camTransform;
+    Vector3 camBasePosition;
 
     private void Start()
     {
         currentSpeed = walkSpeed;
+        camTransform = transform.GetChild(0);
+        camBasePosition = camTransform.localPosition;
     }
 
     // Update is called once per frame
@@ -38,6 +49,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * walkSpeed * Time.deltaTime);
+        bobbingYTimer += bobbingYRate * move.sqrMagnitude * walkSpeed * Time.deltaTime;
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
@@ -51,6 +63,12 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetKey(KeyCode.LeftShift))
         {
             controller.Move(move * sprintSpeed * Time.deltaTime);
+            bobbingYTimer += bobbingYRate * move.sqrMagnitude * sprintSpeed * Time.deltaTime;
         }
+
+        if(bobbingYTimer > 1.0f)
+            bobbingYTimer -= 1.0f;
+
+        camTransform.localPosition = camBasePosition + (Vector3.up * bobbingYCurve.Evaluate(bobbingYTimer) * bobbingYLength);
     }
 }
