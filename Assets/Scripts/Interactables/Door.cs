9a876c7945317c6d33318f9754ca9b7f2d02d485
Sajjad Chandio <157686708+Sajjad-Chandio@users.noreply.github.com>
2024-08-28 02:16:09 -0700

    Implemented Door Looked Interaction System with UI Feedback

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -1,21 +1,52 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class Door : MonoBehaviour, IInteractables
 {
+    public bool isDoorLocked = false;
     bool isOpened = false;
     Animator animator;
+    public Text uiText;
+    string doorLockedMessage = "Need Key To Unlock";
+    public float messageClearDelay = 2.0f;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        if (uiText != null)
+        {
+            uiText.text = "";
+        }
     }
 
     public void Interact()
     {
-        isOpened = !isOpened;
-        transform.position += transform.forward * (isOpened ? 0f : 0f);
-        
-        animator.Play("DoorOpen");
-        //animator.speed = isOpened ? 1 : -1;
+        if (isDoorLocked)
+        {
+            
+            if (uiText != null)
+            {
+                uiText.text = doorLockedMessage;
+                StartCoroutine(ClearMassageAfterDelay());
+            }
+        }
+        else
+        {
+            isOpened = !isOpened;
+
+            animator.Play("DoorOpen");
+
+            if (uiText != null)
+            {
+                StartCoroutine(ClearMassageAfterDelay());
+            }
+        }
+    }
+
+    private IEnumerator ClearMassageAfterDelay()
+    {
+        yield return new WaitForSeconds(messageClearDelay);
+        uiText.text = "";
     }
 }
