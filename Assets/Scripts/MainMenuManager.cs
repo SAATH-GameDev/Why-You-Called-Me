c290b54cdc0445543c7c12e33a4497f12f2b66fa
Shaheryar-vgp <shaheryaryar456@gmail.com>
2024-09-12 18:44:15 +0500

    MainMenu Sequence Updated
    Only Main Menu Panel For testing

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Panels.....")]
+    
+    [SerializeField] private GameObject MainMenuPanel;
+    [SerializeField] private GameObject SettingsPanel;
+    [SerializeField] private GameObject ExitPanel;
+
+    [Header("Buttons.....")]
+    
+    [SerializeField] private Button StartBtn;
+    [SerializeField] private Button SettingsMenu;
+    [SerializeField] private Button Back;
+    [SerializeField] private Button QuitBtn;
+    
+    void Start()
+    {
+       
+        StartBtn.onClick.AddListener(OnStartClicked);
+        SettingsMenu.onClick.AddListener(OpenSettingsPanel);
+        Back.onClick.AddListener(BackToMainMenu);
+        
+       
+        MainMenuPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+        ExitPanel.SetActive(false);
+    }
+
+   
+    void OnStartClicked()
+    {
+        Debug.Log("Start Game");
+        SceneManager.LoadScene("Play"); 
+    }
+
+
+    void OpenSettingsPanel()
+    {
+        SettingsPanel.SetActive(true);
+    }
+
+    void BackToMainMenu()
+    {
+        ExitPanel.SetActive(true);
+       
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
